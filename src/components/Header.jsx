f15193e4557d7c4ce6f22b03af96d443a363876c
Karyna Semenets <insomnia98@ukr.net>
2022-06-13 13:38:33 +0300

    project start

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+      <header className="header">
+        <img
+          src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/buy-one-get-one-free-logo-design-template-08a90705dc649cb0fad947c571902c99_screen.jpg?ts=1610032698"
+          alt="logo"
+        />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://images.ctfassets.net/hrltx12pl8hq/7JnR6tVVwDyUM8Cbci3GtJ/bf74366cff2ba271471725d0b0ef418c/shutterstock_376532611-og.jpg"
+          alt="landscape"
+        />
+      </div>
+      <div>
+        <img
+          src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg"
+          alt="selfie"
+        />
+      </div>
+      <div>My posts</div>
+      <div>New post</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
